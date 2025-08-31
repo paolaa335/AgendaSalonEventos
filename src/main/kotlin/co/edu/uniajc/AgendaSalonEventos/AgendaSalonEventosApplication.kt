d3a938b75ca1166db30e1480bbc9648c4f91f693
Paola <pmambuscay@estudiante.uniajc.edu.co>
2025-08-30 22:06:07 -0500

    proyecto

---
+++ src/main/kotlin/co/edu/uniajc/AgendaSalonEventos/AgendaSalonEventosApplication.kt
@@ -0,0 +1,11 @@
+package co.edu.uniajc.AgendaSalonEventos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgendaSalonEventosApplication
+
+fun main(args: Array<String>) {
+	runApplication<AgendaSalonEventosApplication>(*args)
+}
---
+++ src/main/kotlin/co/edu/uniajc/AgendaSalonEventos/controller/EventoController.kt
@@ -0,0 +1,4 @@
+package co.edu.uniajc.AgendaSalonEventos.controller
+
+class EventoController {
+}
